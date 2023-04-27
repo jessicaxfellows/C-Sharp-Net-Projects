d31b8bc2b03695761c17abf645c4dd09ac509397
Jessica Fellows <jessicaxfellows@gmail.com>
2023-04-27 17:58:44 -0400

    Methods

---
+++ MethodsAssignment/MethodsAssignment/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    public class Math
+    {
+        public int methodOne(int input)
+        {
+            return input + 5;
+        }
+        public int methodTwo(int input)
+        {
+            return input * 5;
+        }
+        public int methodThree(int input)
+        {
+            return input - 5;
+        }
+    }
+}
---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for input, convert input to int
+            Console.WriteLine("What number would you like to perform math operations on?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            
+            Math results = new Math(); //instatiate Math class called results
+            Console.WriteLine(results.methodOne(number));  //call each method and print result
+            Console.WriteLine(results.methodTwo(number));
+            Console.WriteLine(results.methodThree(number));
+
+            Console.ReadLine();
+        }
+    }
+}
