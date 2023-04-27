02c098494369c9d9e1135bfab9a11880d5da34c0
Jessica Fellows <jessicaxfellows@gmail.com>
2023-04-27 18:46:07 -0400

    MainMethod

---
+++ MainMethod/MainMethod/Maths.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Maths
+    {
+        //in a created class, create method that takes in and returns integer after math ops
+        public int methodOne(int input)
+        {
+            return input + 10;
+        }
+
+        //create a second method with same name (overload) take in decimal, math ops and return an int
+        public int methodOne(decimal input)
+        {
+            return decimal.ToInt32(input * 5);
+        }
+
+        //create a third method with the same name that will take in string, convert to int and return int
+        public int methodOne(string input)
+        {
+            return Convert.ToInt32(input)/2;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10; //create integer
+            Maths firstClass = new Maths(); //instantiate a class
+            Console.WriteLine(firstClass.methodOne(x)); //call first method, pass int and display result
+
+            decimal y = 25.0m; //create decimal
+            Maths secondClass = new Maths(); //instantiate class again
+            Console.WriteLine(secondClass.methodOne(y)); //call second method, pass decimal and display result
+
+            string z = "50"; //create string
+            Maths thirdClass = new Maths(); //instantiate class again
+            Console.WriteLine(thirdClass.methodOne(z)); //call third method, pass string and display result
+
+
+            Console.ReadLine();
+        }
+    }
+}
