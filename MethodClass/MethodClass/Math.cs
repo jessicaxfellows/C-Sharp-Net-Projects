24e2e46167379898b12e9d22d05b60f5eba736bc
Jessica Fellows <jessicaxfellows@gmail.com>
2023-05-03 16:39:41 -0400

    Methods

---
+++ MethodClass/MethodClass/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Math
+    {
+        //void method with 2 integers as params
+        public void methodOne(int num1, int num2)
+        {
+            Console.WriteLine("First integer plus 10 is: "+ (10 + num1)); ; //do math ops on first int
+            Console.WriteLine("Second integer is: "+num2); //display second int
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math example = new Math(); //instantiate class
+            int num1 = 15;
+            int num2 = 5;
+            example.methodOne(num1, num2); //call method and pass 2 numbers
+            example.methodOne(num1: 15, num2: 5); //call method by the params name
+
+            Console.ReadLine();
+        }
+    }
+}
