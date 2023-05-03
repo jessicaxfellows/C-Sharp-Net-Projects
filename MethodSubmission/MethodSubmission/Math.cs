62178c4c5e7c61b7ff31bb86ee369a04ce526119
Jessica Fellows <jessicaxfellows@gmail.com>
2023-05-03 16:01:52 -0400

    Methods

---
+++ MethodSubmission/MethodSubmission/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class Math
+    {
+        public int methodOne(int input1, int input2 = 10)
+        {
+            return input1 + input2;
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math newMath = new Math(); //instantiate class
+
+            Console.WriteLine("Enter a number:"); //ask user for first number
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number, this is optional (hit enter either way):"); //ask user for first number
+            int num2; //create a variable for second number if it's inputted
+
+            if(int.TryParse(Console.ReadLine(), out num2)) //check if they inputted a second number 
+            {
+                Console.WriteLine(newMath.methodOne(num1, num2)); //if they did, print result with both numbers
+            }
+            else
+            {
+                Console.WriteLine(newMath.methodOne(num1)); //if not, then print result with only the first number entered
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
