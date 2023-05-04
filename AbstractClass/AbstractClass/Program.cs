47826b98c1982f96985ae49f7bbf2e027926c642
Jessica Fellows <jessicaxfellows@gmail.com>
2023-05-04 17:05:55 -0400

    Abstract

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate employee class, assign first and last, call sayname method
+            Employee example = new Employee();
+            example.FirstName = "Sample";
+            example.LastName = "Student";
+            example.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
