0dba40ad4b091991419c332866a27605a4455763
Jessica Fellows <jessicaxfellows@gmail.com>
2023-06-05 18:46:48 -0400

    Assignment

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print current date and time to console
+            DateTime now = DateTime.Now; //create a datetime obj and access now
+            Console.WriteLine("Current date and time is: " + now);
+
+            //ask user for a number
+            Console.WriteLine("Please enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //create new date time and use "add hours" 
+            DateTime nowPlus = now.AddHours(number);
+
+            //print what time it will be in ^ the number of hours the user inputs
+            Console.WriteLine("In {0} hours, it will be: {1}", number, nowPlus);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //added system IO 
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for a number
+            Console.WriteLine("Please input a number: ");
+            string number = Console.ReadLine();
+            //log that number to a text file 
+            File.WriteAllText(@"C:\Users\jessi\OneDrive\Documents\JessicaFellow.txt", number);
+            //print the text from the file back to the user
+            string numberFile = File.ReadAllText(@"C:\Users\jessi\OneDrive\Documents\JessicaFellow.txt");
+            Console.WriteLine("This is the text being read back from the file: " + numberFile);
+
+            Console.ReadLine();
+        }
+    }
+}
